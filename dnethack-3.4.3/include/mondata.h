619ea5b9dc6b3a9f39644a78514e95cec18fdced
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2013-09-22 12:21:18 -0400

    Dreadblossom swarms should be whirly
    (got left out of previous commit)

--- dnethack-3.4.3/include/mondata.h
+++ dnethack-3.4.3/include/mondata.h
@@ -50,7 +50,8 @@
 #define has_head(ptr)		(((ptr)->mflags1 & M1_NOHEAD) == 0L)
 #define has_horns(ptr)		(num_horns(ptr) > 0)
 #define is_whirly(ptr)		((ptr)->mlet == S_VORTEX || \
-				 (ptr) == &mons[PM_AIR_ELEMENTAL])
+				 (ptr) == &mons[PM_AIR_ELEMENTAL] ||\
+				 (ptr) == &mons[PM_DREADBLOSSOM_SWARM])
 #define flaming(ptr)		((ptr) == &mons[PM_FIRE_VORTEX] || \
 				 (ptr) == &mons[PM_FLAMING_SPHERE] || \
 				 (ptr) == &mons[PM_FIRE_ELEMENTAL] || \
