d63a766b0503085ddf3d4c0bc9c436b4bcbaffbe
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2013-12-06 16:44:01 -0500

    Remove erroneous ifdef tags

--- dnethack-3.4.3/include/you.h
+++ dnethack-3.4.3/include/you.h
@@ -47,9 +47,7 @@ struct u_event {
 
 	Bitfield(invoked,1);		/* invoked Gate to the Sanctum level */
 	Bitfield(gehennom_entered,1);	/* entered Gehennom via Valley */
-#ifdef ELBERETH
 	Bitfield(uhand_of_elbereth,2);	/* became Hand of Elbereth */
-#endif
 	Bitfield(udemigod,1);		/* killed the wiz */
 	Bitfield(ukilled_apollyon,1);		/* killed the angel of the pit.  Lucifer should spawn on Astral */
 	Bitfield(sum_entered,1);		/* entered Sum-of-All */
