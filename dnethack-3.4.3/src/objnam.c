671a0baa32729c0c9b202fb51c526d68f08e724d
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2014-01-16 00:29:53 -0500

    Bugfix: Segfault for generic wishes in dNAO
    There was a segfault for wishing for generic items like "potion" or "food". Was noticed as a segfault when wishing for "holy food"
    
    For reasons that are obscure to me, this segfault only occured in dNAO.

--- dnethack-3.4.3/src/objnam.c
+++ dnethack-3.4.3/src/objnam.c
@@ -2494,6 +2494,7 @@ srch:
 		goto typfnd;
 	}
 	/*This is meant to catch "scrolls of X"*/
+	if(actualn){
 	if(!strncmpi(actualn, "heptagram", 9)){
 		heptagram = TRUE;
 		typ = SCR_WARD;
@@ -2548,6 +2549,7 @@ srch:
 		garuda = TRUE;
 		typ = SCR_WARD;
 		goto typfnd;
+	}
 	}
 	/* Note: not strncmpi.  2 fruits, one capital, one not, are possible. */
 	{
