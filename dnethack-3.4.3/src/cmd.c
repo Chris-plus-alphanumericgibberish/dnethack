4e3904f54238b18b04e0773e4fa5a70bcbc5d0d0
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2014-06-15 15:49:26 -0400

    Remove ward prompt from Engrave

--- dnethack-3.4.3/src/cmd.c
+++ dnethack-3.4.3/src/cmd.c
@@ -2195,8 +2195,8 @@ static const struct func_tab cmdlist[] = {
 	{'D', FALSE, doddrop},
 	{M('d'), FALSE, dodip},
 	{'e', FALSE, doeat},
-	{'E', FALSE, doengward},
-/*	{'E', FALSE, doengrave},*/
+/*	{'E', FALSE, doengward},*/
+	{'E', FALSE, doengrave},
 	{C('e'), TRUE, doseal},
 	{M('e'), TRUE, enhance_weapon_skill},
 	{'f', FALSE, dofire},
