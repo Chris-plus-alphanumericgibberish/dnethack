c0bbe4aee4d1d6be59031e54913796a89be4a9c4
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2014-06-25 09:20:43 -0400

    Fix hostile casts from peaceful casters

--- dnethack-3.4.3/src/monmove.c
+++ dnethack-3.4.3/src/monmove.c
@@ -1023,7 +1023,7 @@ toofar:
 
 		    for (a = &mdat->mattk[0]; a < &mdat->mattk[NATTK]; a++) {
 			if ((a->aatyp == AT_MAGC || a->aatyp == AT_MMGC) && (a->adtyp == AD_SPEL || a->adtyp == AD_CLRC)) {
-			    if (mtmp->mux==u.ux && mtmp->muy==u.uy && couldsee(mtmp->mx, mtmp->my)){
+			    if (mtmp->mux==u.ux && mtmp->muy==u.uy && couldsee(mtmp->mx, mtmp->my) && !mtmp->mpeaceful){
 					if(castmu(mtmp, a, TRUE, TRUE)){
 				tmp = 3;
 						if(mdat != &mons[PM_DEMOGORGON]) break;
