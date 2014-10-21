e1aff7746a3f8449a44fa1af9fd17467a53a3253
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2014-10-21 04:27:06 -0400

    Wizards can't throw volleys of daggers

--- dnethack-3.4.3/src/dothrow.c
+++ dnethack-3.4.3/src/dothrow.c
@@ -115,7 +115,7 @@ int shotlimit;
 	/* Multishot calculations
 	 */
 	skill = objects[obj->otyp].oc_skill;
-	if ((ammo_and_launcher(obj, uwep) || skill == P_DAGGER ||
+	if ((ammo_and_launcher(obj, uwep) || (skill == P_DAGGER && !Role_if(PM_WIZARD)) ||
 			skill == -P_DART || skill == -P_SHURIKEN || skill == -P_BOOMERANG ) &&
 		!(Confusion || Stunned)) {
 	    /* Bonus if the player is proficient in this weapon... */
