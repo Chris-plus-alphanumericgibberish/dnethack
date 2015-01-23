575f063f2849f17b46e5d1f977219252325030e2
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2015-01-22 22:53:53 -0500

    align.h 3.8
    tabbing correction
    
    Add "God Alignment" tokens, for indexing into array that holds divina anger values.
    
    Add conversions for God alignments to altar alignments and player aligments

--- dnethack-3.4.3/include/align.h
+++ dnethack-3.4.3/include/align.h
@@ -16,7 +16,7 @@ typedef struct	align {		/* alignment & record */
 /* bounds for "record" -- respect initial alignments of 10 */
 #define ALIGNLIM	(10L + (moves/200L) - u.ualign.sins)
 
-#define A_NONE	      (-128)	/* the value range of type */
+#define A_NONE		(-128)	/* the value range of type */
 
 #define A_CHAOTIC	(-1)
 #define A_NEUTRAL	 0
@@ -33,6 +33,13 @@ typedef struct	align {		/* alignment & record */
 #define AM_LAWFUL	 4
 #define AM_VOID		 8
 
+#define GA_NONE		 0
+#define GA_CHAOTIC	 1
+#define GA_NEUTRAL	 2
+#define GA_LAWFUL	 3
+#define GA_VOID		 4
+#define GA_NUM		 5
+
 #define AM_MASK		 15
 
 #define AM_SPLEV_CO	 3
@@ -48,6 +55,30 @@ typedef struct	align {		/* alignment & record */
 						  (x)==A_CHAOTIC ? AM_CHAOTIC :\
 						  (x)==A_NEUTRAL ? AM_NEUTRAL :\
 						  (x)==A_LAWFUL ? AM_LAWFUL :\
-						  (x)==A_VOID ? AM_VOID : A_NONE\
+						  (x)==A_VOID ? AM_VOID : AM_NONE\
+						)
+#define Amask2gangr(x)	((aligntyp) ( (x)==AM_NONE ? GA_NONE : \
+									  (x)==AM_CHAOTIC ? GA_CHAOTIC :\
+									  (x)==AM_NEUTRAL ? GA_NEUTRAL :\
+									  (x)==AM_LAWFUL ? GA_LAWFUL :\
+									  (x)==AM_VOID ? GA_VOID : GA_NONE\
+									))
+#define Align2gangr(x)	( (x)==A_NONE ? GA_NONE : \
+						  (x)==A_CHAOTIC ? GA_CHAOTIC :\
+						  (x)==A_NEUTRAL ? GA_NEUTRAL :\
+						  (x)==A_LAWFUL ? GA_LAWFUL :\
+						  (x)==A_VOID ? GA_VOID : GA_NONE\
+						)
+#define Gangr2align(x)	((aligntyp) ( (x)==GA_NONE ? A_NONE : \
+									  (x)==GA_CHAOTIC ? A_CHAOTIC :\
+									  (x)==GA_NEUTRAL ? A_NEUTRAL :\
+									  (x)==GA_LAWFUL ? A_LAWFUL :\
+									  (x)==GA_VOID ? A_VOID : A_NONE\
+									))
+#define Gangr2amask(x)	( (x)==GA_NONE ? AM_NONE : \
+						  (x)==GA_CHAOTIC ? AM_CHAOTIC :\
+						  (x)==GA_NEUTRAL ? AM_NEUTRAL :\
+						  (x)==GA_LAWFUL ? AM_LAWFUL :\
+						  (x)==GA_VOID ? AM_VOID : AM_NONE\
 						)
 #endif /* ALIGN_H */
