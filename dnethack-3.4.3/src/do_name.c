bc5a424b8e46746bf72b5b041ec3d517981e0776
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2015-01-25 15:28:22 -0500

    Healer can see how wounded living targets are
    Also fix the messages

--- dnethack-3.4.3/src/do_name.c
+++ dnethack-3.4.3/src/do_name.c
@@ -796,13 +796,13 @@ boolean called;
 	    Strcat(buf, " the ");
 	    if (do_invis) Strcat(buf, "invisible ");
 		if (mtmp->mflee && mtmp->data == &mons[PM_BANDERSNATCH]) Strcat(buf, "frumious ");
-		if (u.sealsActive&SEAL_MOTHER && !is_undead(mtmp->data)){
-			if(mtmp->mhp == mtmp->mhpmax) Strcat(buf, "uninjured ");
+		if ((u.sealsActive&SEAL_MOTHER && !is_undead(mtmp->data)) || (Role_if(PM_HEALER) && (!nonliving(mtmp->data) || has_blood(mtmp->data))) ){
+			if(mtmp->mhp == mtmp->mhpmax) (has_blood(mtmp->data)) ? Strcat(buf, "uninjured ") : Strcat(buf, "undamaged ");
 			else if(mtmp->mhp >= .9*mtmp->mhpmax) Strcat(buf, "scuffed ");
-			else if(mtmp->mhp >= .5*mtmp->mhpmax) Strcat(buf, "bruised ");
-			else if(mtmp->mhp >= .25*mtmp->mhpmax) Strcat(buf, "bloodied ");
-			else if(mtmp->mhp >= .1*mtmp->mhpmax) Strcat(buf, "badly bloodied ");
-			else if(mtmp->mhp > 0) Strcat(buf, "mortally injured ");
+			else if(mtmp->mhp >= .5*mtmp->mhpmax) (has_blood(mtmp->data)) ?  Strcat(buf, "bruised ") : Strcat(buf, "dented ");
+			else if(mtmp->mhp >= .25*mtmp->mhpmax) (has_blood(mtmp->data)) ? Strcat(buf, "bloodied ") : Strcat(buf, "damaged ");
+			else if(mtmp->mhp >= .1*mtmp->mhpmax) (has_blood(mtmp->data)) ? Strcat(buf, "badly bloodied ") : Strcat(buf, "badly damaged ");
+			else if(mtmp->mhp > 0) (has_blood(mtmp->data)) ? Strcat(buf, "mortally injured ") : Strcat(buf, "critically damaged ");
 		}
 		if(is_drow(mdat)){
 			struct obj *otmp;
@@ -849,13 +849,13 @@ boolean called;
 			name_at_start = TRUE;
 	    } else if (called) {
 			if (mtmp->mflee && mtmp->data == &mons[PM_BANDERSNATCH]) Sprintf(eos(buf), "frumious ");
-			if (u.sealsActive&SEAL_MOTHER && !is_undead(mtmp->data)){
-				if(mtmp->mhp == mtmp->mhpmax) Strcat(buf, "uninjured ");
+			if ((u.sealsActive&SEAL_MOTHER && !is_undead(mtmp->data)) || (Role_if(PM_HEALER) && (!nonliving(mtmp->data) || has_blood(mtmp->data))) ){
+				if(mtmp->mhp == mtmp->mhpmax) (has_blood(mtmp->data)) ? Strcat(buf, "uninjured ") : Strcat(buf, "undamaged ");
 				else if(mtmp->mhp >= .9*mtmp->mhpmax) Strcat(buf, "scuffed ");
-				else if(mtmp->mhp >= .5*mtmp->mhpmax) Strcat(buf, "bruised ");
-				else if(mtmp->mhp >= .25*mtmp->mhpmax) Strcat(buf, "bloodied ");
-				else if(mtmp->mhp >= .1*mtmp->mhpmax) Strcat(buf, "badly bloodied ");
-				else if(mtmp->mhp > 0) Strcat(buf, "mortally injured ");
+				else if(mtmp->mhp >= .5*mtmp->mhpmax) (has_blood(mtmp->data)) ?  Strcat(buf, "bruised ") : Strcat(buf, "dented ");
+				else if(mtmp->mhp >= .25*mtmp->mhpmax) (has_blood(mtmp->data)) ? Strcat(buf, "bloodied ") : Strcat(buf, "damaged ");
+				else if(mtmp->mhp >= .1*mtmp->mhpmax) (has_blood(mtmp->data)) ? Strcat(buf, "badly bloodied ") : Strcat(buf, "badly damaged ");
+				else if(mtmp->mhp > 0) (has_blood(mtmp->data)) ? Strcat(buf, "mortally injured ") : Strcat(buf, "critically damaged ");
 			}
 			if(is_drow(mdat)){
 				struct obj *otmp;
@@ -891,13 +891,13 @@ boolean called;
 	    name_at_start = FALSE;
 	} else {
 		if (mtmp->mflee && mtmp->data == &mons[PM_BANDERSNATCH]) Strcat(buf, "frumious ");
-		if (u.sealsActive&SEAL_MOTHER && !is_undead(mtmp->data)){
-			if(mtmp->mhp == mtmp->mhpmax) Strcat(buf, "uninjured ");
+		if ((u.sealsActive&SEAL_MOTHER && !is_undead(mtmp->data)) || (Role_if(PM_HEALER) && (!nonliving(mtmp->data) || has_blood(mtmp->data))) ){
+			if(mtmp->mhp == mtmp->mhpmax) (has_blood(mtmp->data)) ? Strcat(buf, "uninjured ") : Strcat(buf, "undamaged ");
 			else if(mtmp->mhp >= .9*mtmp->mhpmax) Strcat(buf, "scuffed ");
-			else if(mtmp->mhp >= .5*mtmp->mhpmax) Strcat(buf, "bruised ");
-			else if(mtmp->mhp >= .25*mtmp->mhpmax) Strcat(buf, "bloodied ");
-			else if(mtmp->mhp >= .1*mtmp->mhpmax) Strcat(buf, "badly bloodied ");
-			else if(mtmp->mhp > 0) Strcat(buf, "mortally injured ");
+			else if(mtmp->mhp >= .5*mtmp->mhpmax) (has_blood(mtmp->data)) ?  Strcat(buf, "bruised ") : Strcat(buf, "dented ");
+			else if(mtmp->mhp >= .25*mtmp->mhpmax) (has_blood(mtmp->data)) ? Strcat(buf, "bloodied ") : Strcat(buf, "damaged ");
+			else if(mtmp->mhp >= .1*mtmp->mhpmax) (has_blood(mtmp->data)) ? Strcat(buf, "badly bloodied ") : Strcat(buf, "badly damaged ");
+			else if(mtmp->mhp > 0) (has_blood(mtmp->data)) ? Strcat(buf, "mortally injured ") : Strcat(buf, "critically damaged ");
 		}
 		if(is_drow(mdat)){
 			struct obj *otmp;
