bcd2c92056f18b796065ecb9aa25779545bb9da5
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2015-01-25 15:57:44 -0500

    Shapeshifting weapons should have constant weight

--- dnethack-3.4.3/src/mkobj.c
+++ dnethack-3.4.3/src/mkobj.c
@@ -1199,6 +1199,8 @@ register struct obj *obj;
 {
 	int wt = objects[obj->otyp].oc_weight;
 	if(obj->oartifact == ART_ROD_OF_LORDLY_MIGHT) wt = objects[MACE].oc_weight;
+	else if(obj->oartifact == ART_SCEPTRE_OF_LOLTH) wt = 3*objects[MACE].oc_weight;
+	else if(obj->oartifact == ART_ROD_OF_THE_ELVISH_LORDS) wt = objects[ELVEN_MACE].oc_weight;
 	else if(obj->oartifact == ART_EARTH_CRYSTAL){
 		wt = 80;
 	}
