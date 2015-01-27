a5de36eef83c67712d69bd46885512f2eadead26
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2015-01-27 18:08:13 -0500

    Invert lighting in sokoban for drow

--- dnethack-3.4.3/src/detect.c
+++ dnethack-3.4.3/src/detect.c
@@ -1601,7 +1601,11 @@ sokoban_detect()
 	for (x = 1; x < COLNO; x++)
 	    for (y = 0; y < ROWNO; y++) {
 	    	levl[x][y].seenv = SVALL;
-	    	levl[x][y].waslit = TRUE;
+			if(Race_if(PM_DROW)){
+				set_lit(x,y,0);
+				levl[x][y].waslit = FALSE;
+			}
+	    	else levl[x][y].waslit = TRUE;
 	    	map_background(x, y, 1);
 	    	for (obj = level.objects[x][y]; obj; obj = obj->nexthere)
 	    	    if (obj->otyp == BOULDER)
