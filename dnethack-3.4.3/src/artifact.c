e2a3cd2f5e40411851c347feabe4e664610a024c
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2015-03-08 22:03:11 -0400

    Book of IS can now have it's pages turned as an #invoke effect

--- dnethack-3.4.3/src/artifact.c
+++ dnethack-3.4.3/src/artifact.c
@@ -4087,10 +4087,13 @@ arti_invoke(obj)
 		   }
 		}break;
 		case INFINITESPELLS:{
-			pline("The endless pages of the book cover the material of a spellbook of %s in exhaustive detail.",OBJ_NAME(objects[obj->ovar1]));
-			pline("Following the instructions on the pages, you cast the spell!");
-			spelleffects(0,FALSE,obj->ovar1);
-			if(!rn2(20)){
+			boolean turnpage = yn("Turn to a new page?") == 'y';
+			if(!turnpage){
+				pline("The endless pages of the book cover the material of a spellbook of %s in exhaustive detail.",OBJ_NAME(objects[obj->ovar1]));
+				pline("Following the instructions on the pages, you cast the spell!");
+				spelleffects(0,FALSE,obj->ovar1);
+			}
+			if(turnpage || !rn2(20)){
 				obj->ovar1 = rn2(SPE_BLANK_PAPER - SPE_DIG) + SPE_DIG;
 				pline("The endless pages of the book turn themselves. They settle on a section describing %s.",OBJ_NAME(objects[obj->ovar1]));
 			}
