ee1e231a130a2d782cb1188d3488d6e9804fe143
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2015-04-24 18:13:15 -0400

    Fix compiler bug from left out word

--- dnethack-3.4.3/src/makemon.c
+++ dnethack-3.4.3/src/makemon.c
@@ -3354,7 +3354,7 @@ register int	mmflags;
 		/*At this point, we have FINALLY created the inventory for the initial creature and all its associates, so the global should be unset now.*/
 		curhouse = 0;
 	}
-	if ((ptr->mflags3 & M3_WAITMASK) && !(mmflags & MM_NOWAIT) && !u.udemigod) {
+	if ((ptr->mflags3 & M3_WAITMASK) && !(mmflags & MM_NOWAIT) && !u.uevent.udemigod) {
 		if (ptr->mflags3 & M3_WAITFORU)
 			mtmp->mstrategy |= STRAT_WAITFORU;
 		if (ptr->mflags3 & M3_CLOSE)
