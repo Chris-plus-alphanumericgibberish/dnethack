b0868b865d0cdafcf7e5d2b2723bf3c4883a25b9
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2015-05-26 01:08:21 -0400

    No random levelport traps

--- dnethack-3.4.3/src/mklev.c
+++ dnethack-3.4.3/src/mklev.c
@@ -1365,8 +1365,12 @@ coord *tm;
 		    case SLP_GAS_TRAP:
 			if (lvl < 2) kind = NO_TRAP; break;
 		    case LEVEL_TELEP:
-			if (lvl < 5 || level.flags.noteleport)
-			    kind = NO_TRAP; break;
+			// if (lvl < 5 || level.flags.noteleport)
+			    // kind = NO_TRAP; break;
+			if (lvl < 5)
+			    kind = NO_TRAP;
+			else kind = TRAPDOOR;
+			break;
 		    case SPIKED_PIT:
 			if (lvl < 5) kind = NO_TRAP; break;
 		    case LANDMINE:
