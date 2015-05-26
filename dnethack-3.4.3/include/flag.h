572533d7a358277186d0bd0ed5494824d570a8e5
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2015-05-26 01:05:56 -0400

    Flag for obscure role names

--- dnethack-3.4.3/include/flag.h
+++ dnethack-3.4.3/include/flag.h
@@ -238,6 +238,7 @@ struct instance_flags {
 	boolean  quiver_fired;
        boolean  pickup_thrown;
     boolean role_obj_names;
+    boolean obscure_role_obj_names;
     boolean dnethack_start_text;
     boolean dnethack_dungeon_colors;
 /*
