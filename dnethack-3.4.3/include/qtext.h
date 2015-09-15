700e44e4b5ceaff7bcc0bf92c599680f7dd68556
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2015-09-15 15:25:38 -0400

    Increase size of qtext to enable Anachrononaut quest text

--- dnethack-3.4.3/include/qtext.h
+++ dnethack-3.4.3/include/qtext.h
@@ -5,7 +5,7 @@
 #ifndef QTEXT_H
 #define QTEXT_H
 
-#define N_HDR	26		/* Maximum number of categories */
+#define N_HDR	27		/* Maximum number of categories */
 				/* (i.e., num roles + 1) */
 #define LEN_HDR 3		/* Maximum length of a category name */
 
