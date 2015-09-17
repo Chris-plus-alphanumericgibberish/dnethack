31799233d697f5f8cc02789af5f37bb187ad5669
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2015-09-17 16:25:05 -0400

    Lighten the chaos crystals

--- dnethack-3.4.3/src/mkobj.c
+++ dnethack-3.4.3/src/mkobj.c
@@ -1311,21 +1311,21 @@ register struct obj *obj;
 		wt = 80;
 	}
 	else if(obj->oartifact == ART_WATER_CRYSTAL){
-		wt = 40;
+		wt = 20;
 	}
 	else if(obj->oartifact == ART_FIRE_CRYSTAL){
-		wt = 20;
+		wt = 10;
 	}
 	else if(obj->oartifact == ART_AIR_CRYSTAL){
-		wt = 20;
+		wt = 10;
 	}
 	else if(obj->oartifact == ART_BLACK_CRYSTAL){
-		wt = 40; //200 total
+		wt = 30; //150 total
 	}
 	else if(obj->oartifact == ART_DRAGON_PLATE || obj->oartifact == ART_CHROMATIC_DRAGON_SCALES){
 		wt =  (int)(wt * 1.5); //225
 	} else if(obj->oartifact == ART_TREASURY_OF_PROTEUS){
-		wt =  150; /* Same as a crystal ball (ie, the Orb of Weight) */
+		wt =  50; /* Same as a crystal ball (ie, the Orb of Weight) */
 	} else if(obj->oartifact == ART_WAR_MASK_OF_DURIN){
 		obj->corpsenm = PM_DWARF;
 	}
