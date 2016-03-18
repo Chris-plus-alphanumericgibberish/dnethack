279f737eaa1ea5aca60856bd014d44f2d691bd08
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2016-03-18 10:14:44 -0400

    3.12.3: Partial reform of pets

--- dnethack-3.4.3/include/patchlevel.h
+++ dnethack-3.4.3/include/patchlevel.h
@@ -30,8 +30,9 @@
  * 3.10.0: belated increment of minor version, Bugfixed bard, bards gain exp by watching their pets level up.
  * 3.10.1: Expand size of ose buffers, to 'fix' buffer overflow.
  * 3.10.2: Expand size of mnum and quest artifact listing to int instead of short int.
+ * 3.10.3: Partial reform of charm monster and monster summoning.
  */
-#define PATCHLEVEL	2
+#define PATCHLEVEL	3
 /*
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
