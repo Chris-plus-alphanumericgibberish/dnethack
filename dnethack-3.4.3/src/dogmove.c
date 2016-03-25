987e6187a5e4712930bcc9fc170938f8679778bd
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2016-03-24 22:53:59 -0400

    Fix counter-hording behavior, was meant to be pet's inventory, not yours

--- dnethack-3.4.3/src/dogmove.c
+++ dnethack-3.4.3/src/dogmove.c
@@ -160,10 +160,14 @@ boolean check_if_better;
 	    /* Check if you've got one.
 	       If you DO, don't hoard it. */
             register struct obj *otmp2;
-	    for(otmp2 = invent; otmp2; otmp2 = otmp2->nobj)
-	        if (otmp->otyp == otmp2->otyp ||
+	    for(otmp2 = mtmp->minvent; otmp2; otmp2 = otmp2->nobj)
+	        if (otmp2 != otmp && (otmp->otyp == otmp2->otyp ||
 	            (otmp->otyp == FOOD_CLASS && otmp2->otyp == FOOD_CLASS))
-	            return FALSE;
+			) return FALSE;
+//         /* Check if the player has one. If you don't, don't hoard it. */
+//        for (otmp2 = invent; otmp2; otmp2 = otmp2->nobj)
+//			if (otmp->otyp == otmp2->otyp ||
+//	            (otmp->otyp == FOOD_CLASS && otmp2->otyp == FOOD_CLASS))
 		return TRUE;
 	}
     }
