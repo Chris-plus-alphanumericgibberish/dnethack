3537bfc80bb1ca525eccaadf1b75ab7af72da081
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2016-08-19 16:56:15 -0400

    Iron and curse sensitivity are now a thing

--- dnethack-3.4.3/src/dog.c
+++ dnethack-3.4.3/src/dog.c
@@ -876,6 +876,12 @@ rock:
 	    if (hates_silver(mon->data) &&
 		objects[obj->otyp].oc_material == SILVER)
 		return(TABU);
+	    if (hates_iron(mon->data) &&
+		objects[obj->otyp].oc_material == IRON)
+		return(TABU);
+	    if (hates_unholy(mon->data) &&
+		obj->cursed)
+		return(TABU);
 	    if (herbi && (obj->otyp == SHEAF_OF_HAY || obj->otyp == SEDGE_HAT))
 		return CADAVER;
 	    if (mon->data == &mons[PM_GELATINOUS_CUBE] && is_organic(obj))
