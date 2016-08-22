68c22c9116677e4d2e13fc729c2a7fc687b81be8
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2016-08-22 16:54:48 -0400

    The rogue gear spirits are small now

--- dnethack-3.4.3/src/uhitm.c
+++ dnethack-3.4.3/src/uhitm.c
@@ -1117,7 +1117,7 @@ int thrown;
 		    if (mdat->mlet == S_SHADE && !(objects[obj->otyp].oc_material == SILVER || arti_silvered(obj) || u.sealsActive&SEAL_CHUPOCLOPS))
 				tmp = 0;
 		    else if(obj->oartifact == ART_LIECLEAVER) tmp = 2*(rnd(12) + rnd(10) + obj->spe);
-		    else if(obj->oartifact == ART_ROGUE_GEAR_SPIRITS) tmp = 2*(rnd(bigmonst(mon->data) ? 3 : 6) + obj->spe);
+		    else if(obj->oartifact == ART_ROGUE_GEAR_SPIRITS) tmp = 2*(rnd(bigmonst(mon->data) ? 2 : 5) + obj->spe);
 		    else if(obj->oclass == SPBOOK_CLASS && u.sealsActive&SEAL_PAIMON) tmp = rnd(spiritDsize()) + objects[obj->otyp].oc_level;
 			else tmp = rnd(2);
 			
