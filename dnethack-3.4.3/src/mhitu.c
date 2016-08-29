111e58d8e5bb88cadb594dc1cac12f13812c783c
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2016-08-29 13:19:44 -0400

    Oona doesn't summon minions

--- dnethack-3.4.3/src/mhitu.c
+++ dnethack-3.4.3/src/mhitu.c
@@ -587,6 +587,7 @@ mattacku(mtmp)
 
 /*	Special demon/minion handling code */
 	if(!mtmp->cham && (is_demon(mdat) || is_minion(mdat)) && !range2
+	   && mtmp->data != &mons[PM_OONA]
 	   && mtmp->data != &mons[PM_BALROG]
 	   && mtmp->data != &mons[PM_DURIN_S_BANE]
 	   && mtmp->data != &mons[PM_SUCCUBUS]
