0117988a02e05e3a3d3fb6dbc0e3149c7ec902c5
Kyle Shanafelt <foxcoll@live.com>
2016-09-04 23:59:42 -0700

    Clean comments out of nhsetup.bat

--- dnethack-3.4.3/sys/win10/nhsetup.bat
+++ dnethack-3.4.3/sys/win10/nhsetup.bat
@@ -16,41 +16,6 @@ if not exist ..\..\util\makedefs.c goto :err_dir
 if not exist ..\..\sys\winnt\winnt.c goto :err_dir
 echo Directories look ok.
 
-:do_tty
-@rem if NOT exist ..\..\binary\*.* mkdir ..\..\binary
-@rem if NOT exist ..\..\binary\license copy ..\..\dat\license ..\..\binary\license >nul
-@REM echo Copying Microsoft Makefile - Makefile.msc to ..\..\src\Makefile.
-@REM if NOT exist ..\..\src\Makefile goto :domsc
-@REM copy ..\..\src\Makefile ..\..\src\Makefile-orig >nul
-@REM echo      Your existing
-@REM echo           ..\..\src\Makefile
-@REM echo      has been renamed to
-@REM echo           ..\..\src\Makefile-orig
-@REM :domsc
-@REM copy Makefile.msc ..\..\src\Makefile >nul
-@REM echo Microsoft Makefile copied ok.
-
-@REM echo Copying Borland Makefile - Makefile.bcc to ..\..\src\Makefile.bcc
-@REM if NOT exist ..\..\src\Makefile.bcc goto :dobor
-@REM copy ..\..\src\Makefile.bcc ..\..\src\Makefile.bcc-orig >nul
-@REM echo      Your existing 
-@REM echo           ..\..\src\Makefile.bcc 
-@REM echo      has been renamed to 
-@REM echo           ..\..\src\Makefile.bcc-orig
-@REM :dobor
-@REM copy Makefile.bcc ..\..\src\Makefile.bcc >nul
-@REM echo Borland Makefile copied ok.
-
-@REM echo Copying MinGW Makefile - Makefile.gcc to ..\..\src\Makefile.gcc
-@REM if NOT exist ..\..\src\Makefile.gcc goto :dogcc
-@REM copy ..\..\src\Makefile.gcc ..\..\src\Makefile.gcc-orig >nul
-@REM echo      Your existing
-@REM echo           ..\..\src\Makefile.gcc
-@REM echo      has been renamed to
-@REM echo           ..\..\src\Makefile.gcc-orig
-@REM :dogcc
-@REM copy Makefile.gcc ..\..\src\Makefile.gcc >nul
-@REM echo MinGW Makefile copied ok.
 
 :do_win
 if not exist ..\..\win\win10\nethack.sln goto :err_win
@@ -64,22 +29,6 @@ if NOT exist ..\..\binary\license copy ..\..\dat\license ..\..\binary\license >n
 if NOT exist ..\..\build\*.* echo Creating ..\..\build directory
 if NOT exist ..\..\build\*.* mkdir ..\..\build
 for %%G in (..\..\win\win10\*.vcxproj ..\..\win\win10\*.vcxproj.filters ..\..\win\win10\*.mak) Do mklink /H ..\..\build\%%~nxG %%G >nul
-@REM for %%G in (..\..\win\win10\*.vcxproj.filters) Do mklink /H ..\..\build\%%~nxG %%G >nul
-@REM  copy ..\..\win\win32\dgncomp.dsp   ..\..\build >nul
-@REM copy ..\..\win\win32\dgnstuff.dsp  ..\..\build >nul
-@REM copy ..\..\win\win32\dgnstuff.mak  ..\..\build >nul
-@REM copy ..\..\win\win32\dlb_main.dsp  ..\..\build >nul
-@REM copy ..\..\win\win32\levcomp.dsp   ..\..\build >nul
-@REM copy ..\..\win\win32\levstuff.dsp  ..\..\build >nul
-@REM copy ..\..\win\win32\levstuff.mak  ..\..\build >nul
-@REM copy ..\..\win\win32\makedefs.dsp  ..\..\build >nul
-@REM copy ..\..\win\win32\recover.dsp   ..\..\build >nul
-@REM copy ..\..\win\win32\tile2bmp.dsp  ..\..\build >nul
-@REM copy ..\..\win\win32\tiles.dsp     ..\..\build >nul
-@REM copy ..\..\win\win32\tiles.mak     ..\..\build >nul
-@REM copy ..\..\win\win32\tilemap.dsp   ..\..\build >nul
-@REM copy ..\..\win\win32\uudecode.dsp   ..\..\build >nul
-@REM copy ..\..\win\win32\nethackw.dsp   ..\..\build >nul
 
 goto :done
 
