46e060251600a1c6aca3d7424d6f98c6a4bb7ea9
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2016-10-05 20:56:26 -0400

    Tweak: Jumpsuits are light armor

--- dnethack-3.4.3/include/obj.h
+++ dnethack-3.4.3/include/obj.h
@@ -358,7 +358,7 @@ struct obj {
 #define is_suit(otmp)	(otmp->oclass == ARMOR_CLASS && \
 			 objects[otmp->otyp].oc_armcat == ARM_SUIT)
 #define is_light_armor(otmp)	((otmp)->otyp == DWARVISH_MITHRIL_COAT || (otmp)->otyp == ELVEN_MITHRIL_COAT || \
-			(otmp)->otyp == LEATHER_JACKET || (otmp)->otyp == ELVEN_TOGA || (otmp)->otyp == BLACK_DRESS)
+			(otmp)->otyp == JUMPSUIT || (otmp)->otyp == LEATHER_JACKET || (otmp)->otyp == ELVEN_TOGA || (otmp)->otyp == BLACK_DRESS)
 #define is_medium_armor(otmp)	((otmp)->otyp == BRONZE_PLATE_MAIL || (otmp)->otyp == DROVEN_CHAIN_MAIL || \
 			(otmp)->otyp == CHAIN_MAIL || (otmp)->otyp == SCALE_MAIL || (otmp)->otyp == STUDDED_LEATHER_ARMOR || \
 			(otmp)->otyp == LEATHER_ARMOR || (otmp)->otyp == BANDED_MAIL || (otmp)->otyp == NOBLE_S_DRESS || \
