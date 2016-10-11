838ae321b87c24224fac87d29117b188d2f119c9
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2016-10-11 14:31:09 -0400

    Bugfix/Tweak: Monsters shouldn't get extra attacks just because they miss.
    Note that this means that, since monsters prioritize pets over you in melee, fighting next to your pet just got a HUGE buff

--- dnethack-3.4.3/src/mhitm.c
+++ dnethack-3.4.3/src/mhitm.c
@@ -168,13 +168,14 @@ fightm(mtmp)		/* have monsters fight each other */
 		     * to allow monsters that resist conflict to respond.
 		     */
 		    if ((result & MM_HIT) && !(result & MM_DEF_DIED) &&
-			rn2(4) && mon->movement >= NORMAL_SPEED ) {
+			rn2(4) && mon->movement >= NORMAL_SPEED && mtmp->data != &mons[PM_NURSE]) {
 				mon->movement -= NORMAL_SPEED;
 				notonhead = 0;
 				(void) mattackm(mon, mtmp);	/* return attack */
 		    }
-
-		    return ((result & MM_HIT) ? 1 : 0);
+			
+			//If was conflict and a miss, can continue to attack.  Otherwise, ignore you.
+		    return (((result & MM_HIT) || !conflict) ? 1 : 0);
 		}
 	    }
 	}
