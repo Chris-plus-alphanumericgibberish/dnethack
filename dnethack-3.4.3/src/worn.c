91882f898a963231bb6a5cee3113ae218d991580
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2016-10-13 01:15:27 -0400

    Variable AC for angel high lords

--- dnethack-3.4.3/src/worn.c
+++ dnethack-3.4.3/src/worn.c
@@ -457,6 +457,9 @@ struct monst *mon;
 	if(mon->data == &mons[PM_ASMODEUS] && base < -9) base = -9 + AC_VALUE(base+9);
 	else if(mon->data == &mons[PM_PALE_NIGHT] && base < -6) base = -6 + AC_VALUE(base+6);
 	else if(mon->data == &mons[PM_BAALPHEGOR] && base < -8) base = -8 + AC_VALUE(base+8);
+	else if(mon->data == &mons[PM_ZAPHKIEL] && base < -8) base = -8 + AC_VALUE(base+8);
+	else if(mon->data == &mons[PM_TULANI_QUEEN] && base < -6) base = -6 + AC_VALUE(base+6);
+	else if(mon->data == &mons[PM_ETERNAL_LIGHT] && base < -6) base = -6 + AC_VALUE(base+6);
 	else if(mon->data == &mons[PM_CHOKHMAH_SEPHIRAH]){
 		base -= u.chokhmah;
 	}
