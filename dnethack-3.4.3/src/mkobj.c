09fbf7decaaeac49039944f1692870937a6ceebc
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2017-07-02 11:23:36 -0400

    Annulus should weigh as much as the bell of opening, not a BFG

--- dnethack-3.4.3/src/mkobj.c
+++ dnethack-3.4.3/src/mkobj.c
@@ -1548,7 +1548,7 @@ register struct obj *obj;
 {
 	int wt = objects[obj->otyp].oc_weight;
 	if(obj->oartifact == ART_ROD_OF_LORDLY_MIGHT) wt = objects[MACE].oc_weight;
-	else if(obj->oartifact == ART_ANNULUS) wt = objects[BFG].oc_weight;
+	else if(obj->oartifact == ART_ANNULUS) wt = objects[BELL_OF_OPENING].oc_weight;
 	else if(obj->oartifact == ART_SCEPTRE_OF_LOLTH) wt = 3*objects[MACE].oc_weight;
 	else if(obj->oartifact == ART_ROD_OF_THE_ELVISH_LORDS) wt = objects[ELVEN_MACE].oc_weight;
 	else if(obj->oartifact == ART_EARTH_CRYSTAL){
