b83e57de63578c1ad8049beffe83f71b028add92
Chris-plus-alphanumericgibberish <chrisMorale@gmail.com>
2017-07-23 00:43:08 -0400

    Send peaceful glyph to display

--- dnethack-3.4.3/src/display.c
+++ dnethack-3.4.3/src/display.c
@@ -437,6 +437,12 @@ display_monster(x, y, mon, sightflags, worm_tail)
 			petnum_to_glyph(PM_LONG_WORM_TAIL);
 	    else
 		num = pet_to_glyph(mon);
+	} else if (mon->mpeaceful && !Hallucination) {
+	    if (worm_tail) num = mon->data == &mons[PM_HUNTING_HORROR] ?
+			peacenum_to_glyph(PM_HUNTING_HORROR_TAIL):
+			peacenum_to_glyph(PM_LONG_WORM_TAIL);
+	    else
+		num = peace_to_glyph(mon);
 	} else if (is_derived_undead_mon(mon) && !Hallucination) {
 	    if (worm_tail) num = mon->data == &mons[PM_HUNTING_HORROR] ?
 			zombienum_to_glyph(PM_HUNTING_HORROR_TAIL):
