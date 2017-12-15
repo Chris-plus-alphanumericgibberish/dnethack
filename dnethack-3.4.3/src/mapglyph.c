aeb5a7772aa1b9dba644d4b037a957296d2204db
ChrisANG <ChrisANG@atdotcom.com>
2017-12-15 11:45:59 -0500

    Rooms on the Neutral quest should have brown floors

--- dnethack-3.4.3/src/mapglyph.c
+++ dnethack-3.4.3/src/mapglyph.c
@@ -235,9 +235,15 @@ unsigned *ospecial;
 					}
 					if(offset == S_drkroom || offset == S_litroom){
 						if(u.uz.dlevel < spire_level.dlevel &&
-							u.uz.dlevel > gatetown_level.dlevel)
-							color = (offset == S_litroom) ? CLR_BRIGHT_GREEN : CLR_GREEN;
-						else color = CLR_BROWN;
+							u.uz.dlevel > gatetown_level.dlevel){
+								if (*in_rooms(x,y, SHOPBASE)
+									|| *in_rooms(x,y, TEMPLE)
+									|| *in_rooms(x,y, BARRACKS)
+									|| *in_rooms(x,y, COURT)
+								) color = (offset == S_litroom) ? CLR_BROWN : CLR_BLACK;
+								else color = (offset == S_litroom) ? CLR_BRIGHT_GREEN : CLR_GREEN;
+							}
+						else color = (offset == S_litroom) ? CLR_BROWN : CLR_BLACK;
 					}
 				}
 				else if(Is_rlyeh(&u.uz)){
