77e2203dcf42aae2b6d8685da7db699a816ab3a2
ChrisANG <ChrisANG@atdotcom.com>
2018-06-02 10:02:58 -0400

    Alignment adjustments for femHDrNob artifacts

--- dnethack-3.4.3/include/artilist.h
+++ dnethack-3.4.3/include/artilist.h
@@ -445,7 +445,7 @@ A("The Lifehunt Scythe",			SCYTHE, /*Needs encyc entry*/
 	(SPFX_RESTR|SPFX_DEFN|SPFX_BEHEAD),0, //Can only behead creatures that don't know where you are.
 	0 /*Monster Symbol*/, 0 /*MM*/, 0 /*MT*/, 0 /*MB*/, 0 /*MG*/, 0 /*MA*/, 0 /*MV*/,
 	PHYS(6,6),	DRLI(0,0),	COLD(0,0), //Actually only applies vs living or undead creatures
-	INVIS, A_CHAOTIC, NON_PM, NON_PM, 4000L, 
+	INVIS, A_CHAOTIC, NON_PM, NON_PM, 4000L, //Note: Has Str and Dex scaling for +16 max
 	SPFX2_STLTH,0,0),
 
 A("The Holy Moonlight Sword",		LONG_SWORD, /*Needs encyc entry*/
@@ -1237,21 +1237,21 @@ A("The Profaned Greatscythe",			SCYTHE, /*Needs encyc entry*/
 	(SPFX_RESTR|SPFX_DEFN|SPFX_ATTK),0,
 	0 /*Monster Symbol*/, 0 /*MM*/, 0 /*MT*/, 0 /*MB*/, 0 /*MG*/, 0 /*MA*/, 0 /*MV*/,
 	FIRE(8,30),	NO_DFNS,	FIRE(0,0),
-	0, A_NONE, PM_NOBLEMAN, 0, 4000L, 
+	0, A_LAWFUL, PM_NOBLEMAN, 0, 4000L, 
 	0,0,0),
 
 A("Friede's Scythe",			SCYTHE, /*Needs encyc entry*/
 	(SPFX_RESTR|SPFX_DEFN|SPFX_ATTK),0,
 	0 /*Monster Symbol*/, 0 /*MM*/, 0 /*MT*/, 0 /*MB*/, 0 /*MG*/, 0 /*MA*/, 0 /*MV*/,
 	STUN(6,6),	NO_DFNS,	CARY(AD_MAGM),//Cold-and-magic
-	0, A_NONE, PM_NOBLEMAN, 0, 4000L, 
+	0, A_LAWFUL, PM_NOBLEMAN, 0, 4000L, //Note: Has Str/2, Dex, and Int scaling for +20 (ouch) max
 	SPFX2_SHINING,0,0),
 
 A("Yorshka's Spear",			SPEAR, /*Needs encyc entry*/
 	(SPFX_RESTR|SPFX_DEFN),0,
 	0 /*Monster Symbol*/, 0 /*MM*/, 0 /*MT*/, 0 /*MB*/, 0 /*MG*/, 0 /*MA*/, 0 /*MV*/,
-	NO_ATTK,	NO_DFNS,	NO_CARY,//Cold-and-magic
-	0, A_NONE, PM_NOBLEMAN, 0, 4000L, 
+	NO_ATTK,	NO_DFNS,	NO_CARY,//Note: Has Str, Dex, and Wis scaling for +24 (ouch) max
+	0, A_LAWFUL, PM_NOBLEMAN, 0, 4000L,//Note: Drains energy/sets special ability cooldowns 
 	0,0,0),
 
 A("The Mitre of Holiness",	HELM_OF_BRILLIANCE,
