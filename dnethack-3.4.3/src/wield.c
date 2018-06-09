35417d31722adecf5472c8686bb74eca12b144fc
ChrisANG <ChrisANG@atdotcom.com>
2018-06-09 13:20:23 -0400

    Fix bad safelim code

--- dnethack-3.4.3/src/wield.c
+++ dnethack-3.4.3/src/wield.c
@@ -787,7 +787,7 @@ register int amount;
 	    return(1);
 	}
 	/* there is a (soft) upper and lower limit to uwep->spe */
-	safelim = uwep->otyp == 5;
+	safelim = 5;
 	if(((uwep->spe > safelim && amount >= 0) || (uwep->spe < -safelim && amount < 0))
 								&& rn2(3) && uwep->oartifact != ART_ROD_OF_SEVEN_PARTS
 								&& uwep->oartifact != ART_PEN_OF_THE_VOID
@@ -836,7 +836,7 @@ register int amount;
 
 	/* an elven magic clue, cookie@keebler */
 	/* elven weapons vibrate warningly when enchanted beyond a limit */
-	if ((uwep->otyp == CRYSTAL_SWORD ? (uwep->spe > 8) : (uwep->spe > 5)) 
+	if ((uwep->spe > 5) 
 		&& uwep->oartifact != ART_PEN_OF_THE_VOID && uwep->oartifact != ART_ANNULUS &&
 		(is_elven_weapon(uwep) || uwep->oartifact || !rn2(7)) &&
 		uwep->oartifact != ART_ROD_OF_SEVEN_PARTS
