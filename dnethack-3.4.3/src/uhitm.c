dcc8af8f3799b9a0a810075b377ced4119a26a31
ChrisANG <ChrisANG@atdotcom.com>
2018-07-03 20:30:28 -0400

    Remove lingering refference to a new variable introduced out of confusion

--- dnethack-3.4.3/src/uhitm.c
+++ dnethack-3.4.3/src/uhitm.c
@@ -796,7 +796,7 @@ struct attack *uattk;
 	    notonhead = (mon->mx != x || mon->my != y);
 	    malive = hmon(mon, uwep, 0);
 	    /* this assumes that Stormbringer was uwep not uswapwep */ 
-	    if (malive && (u.twoweap && !(uwep && uwep->otyp == STILETTOS)) && !stormbringer_override &&
+	    if (malive && (u.twoweap && !(uwep && uwep->otyp == STILETTOS)) && !override_confirmation &&
 		    m_at(x, y) == mon
 		) {
 			malive = hmon(mon, uswapwep, 0);
