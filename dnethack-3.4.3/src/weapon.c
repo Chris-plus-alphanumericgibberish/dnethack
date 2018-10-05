0194073c8b988f3d09ec1f66e17644423f9f2d72
ChrisANG <ChrisANG@atdotcom.com>
2018-10-05 17:41:26 -0400

    Order of ops fix

--- dnethack-3.4.3/src/weapon.c
+++ dnethack-3.4.3/src/weapon.c
@@ -1289,7 +1289,7 @@ lightsaber_form_sdie:
 	    if (bonus > 1 && otmp->oartifact && spec_dbon(otmp, mon, 100) >= 100)
 		bonus = (bonus + 1) / 2;
 		
-		if(mon && ((resists_all(ptr) && !narrow_spec_applies(otmp, mon))) || resist_attacks(ptr)){
+		if(mon && (((resists_all(ptr) && !narrow_spec_applies(otmp, mon))) || resist_attacks(ptr))){
 			tmp /= 4;
 			if(!flags.mon_moving && !youdefend && warnedptr != ptr){
 				pline("Weapons are ineffective against %s.", mon_nam(mon));
