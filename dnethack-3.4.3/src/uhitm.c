a0fb9fe56de4b25c8a104a4da89abe6e4b7ec932
ChrisANG <ChrisANG@atdotcom.com>
2018-10-05 17:40:58 -0400

    Warning fix

--- dnethack-3.4.3/src/uhitm.c
+++ dnethack-3.4.3/src/uhitm.c
@@ -4785,7 +4785,7 @@ dobpois:
 	    if(!mhit) break; //didn't draw blood, forget it.
 		if(mon->data == &mons[PM_LEGION]){
 			int n = rnd(4);
-			for(n; n>0; n--) rn2(7) ? makemon(mkclass(S_ZOMBIE, G_NOHELL|G_HELL), mon->mx, mon->my, NO_MINVENT|MM_ADJACENTOK|MM_ADJACENTSTRICT): 
+			for(; n>0; n--) rn2(7) ? makemon(mkclass(S_ZOMBIE, G_NOHELL|G_HELL), mon->mx, mon->my, NO_MINVENT|MM_ADJACENTOK|MM_ADJACENTSTRICT): 
 									  makemon(&mons[PM_LEGIONNAIRE], mon->mx, mon->my, NO_MINVENT|MM_ADJACENTOK|MM_ADJACENTSTRICT);
 		} else {
 			if(mon->mhp > .75*mon->mhpmax) makemon(&mons[PM_LEMURE], mon->mx, mon->my, MM_ADJACENTOK);
