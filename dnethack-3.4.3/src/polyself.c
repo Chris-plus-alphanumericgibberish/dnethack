e44892a31e2414adc59df63030b2bda142e0eec4
ChrisANG <ChrisANG@atdotcom.com>
2019-09-12 13:14:43 -0400

    Add missing .

--- dnethack-3.4.3/src/polyself.c
+++ dnethack-3.4.3/src/polyself.c
@@ -1496,7 +1496,7 @@ doandroid()
 			return 0;
 	    }
 	    if(!recharge(otmp, 0))
-			You("recharged %s", the(xname(otmp)));
+			You("recharged %s.", the(xname(otmp)));
 		u.uen -= 10;
 	    update_inventory();
 		return 1;
